5554f3098c58feb8857d70a3b52574a9d9b2d1b2
Erik Wendel <erik@wendel.no>
2015-09-03 09:16:55 +0200

    removed norwegian task descriptions

--- tasks/1/script.js
+++ tasks/1/script.js
@@ -1,13 +1,3 @@
-/* 
-
-## Oppgave 1: Bli kjent med komponenter
-
-	Lag en enkel React-komponent som kun skriver ut `Hello World` til DOM-en.
-
-	#### Tips
-
-	Interessante metoder: 
-	React.createClass, React.render
-
-	Husk JSX-headsup i første linje:<br>
-	/** @jsx React.DOM */
+/*
+    Exercise 1 
+*/
--- tasks/3/script.js
+++ tasks/3/script.js
@@ -1,19 +1,3 @@
 /*
-
-## Oppgave 3: Tilstandsfulle komponenter: Timer
-
-	Lag en komponent Timer som skriver ut mengden tid som er gått siden komponenten ble startet. Eksempel:
-
-	I was started 7.8 seconds ago
-
-	Komponenten skal oppdatere seg ti ganger i sekundet, og komponenten skal rydde opp etter seg når den unmountes.
-
-	#### Tips
-
-	Interessante metoder:
-	setInterval, clearInterval
-
-	Lifecycle hooks:
-	componentDidMount, componentWillUnmount
-
+    Exercise 3
 */
