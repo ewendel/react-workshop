f606b3b6f1a09e8864ac8e7012d3b894908866e8
Erik Wendel <erik@wendel.no>
2016-01-30 17:11:48 +0100

    changed to using the new map

--- case/solution/js/components/TweetMap.jsx
+++ case/solution/js/components/TweetMap.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var _ = require('lodash');
 
-var Map = require('google-map-react');
+import { GoogleMapLoader, GoogleMap, Marker } from "react-google-maps";
 
 var TweetMap = React.createClass({
     displayName: 'TweetMap',
@@ -10,25 +10,31 @@ var TweetMap = React.createClass({
 
         var props = this.props;
 
-        // var markers = this.props.tweets.map(function(t) {
-        //     var callback = function() {
-        //         props.showTweet(t.id);
-        //     }
-        //     var icon = t === props.currentTweet ?
-        //       'http://maps.google.com/mapfiles/ms/icons/green-dot.png' :
-        //       'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+        var markers = this.props.tweets.map(function(t) {
+            var callback = function() {
+                props.showTweet(t.id);
+            }
+            var icon = t === props.currentTweet ?
+              'http://maps.google.com/mapfiles/ms/icons/green-dot.png' :
+              'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
 
-        //     return <Marker
-        //         onClick={ callback }
-        //         icon={ icon }
-        //         position={new GoogleMapsAPI.LatLng(t.geo.coordinates[0], t.geo.coordinates[1])} />
-        // });
+            return <Marker
+                key={ t.id }
+                onClick={ callback }
+                icon={ icon }
+                position={{ lat: t.geo.coordinates[0], lng: t.geo.coordinates[1] }} />
+        });
 
         return <div className="tweet-map">
-                  <Map
-                      defaultZoom={3}
-                      defaultCenter={{ lat: 30.675226, lng: -35.051272 }}>
-                  </Map>
+                <GoogleMapLoader
+                    googleMapElement={
+                      <GoogleMap
+                        defaultZoom={3}
+                        defaultCenter={{ lat: 30.675226, lng: -35.051272 }}>
+                        { markers }
+                      </GoogleMap>
+                    }
+                  />
                 </div>
     }
 });
