f5e69a4169702c65214fc9f132dbc102702decf1
Erik Wendel <erik@wendel.no>
2016-02-09 01:17:39 -0800

    React -> ReactDOM

--- case/task/js/app.jsx
+++ case/task/js/app.jsx
@@ -1,8 +1,9 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 var App = require('./components/App');
 
-React.render(
+ReactDOM.render(
     <App />,
     document.getElementById('app')
 );
