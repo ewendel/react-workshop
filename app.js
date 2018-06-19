a7c0f9ca9727608b5217d34f60f31a461f2fd08b
Kristofer Selbekk <selbeezy@gmail.com>
2018-06-19 23:08:06 +0200

    Update app.js to also host the solutions at /solutions

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var port = 3000;
 
 // app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, 'tasks'))); //  "public" off of current is root
+app.use('/solutions', express.static(path.join(__dirname, 'solutions')));
 
 app.listen(port, function() {
     console.log('The tasks server has been started on port ' + port);
